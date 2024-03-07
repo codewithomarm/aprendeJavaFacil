b22f828427ef0993d2dc351dd3bbff5ce2a5a165
codewithomarm <codewithomarm@gmail.com>
2024-03-07 11:33:18 -0500

    Actualización de archivos de clases

---
+++ Clase 5 - Metodo main y Hola Mundo en Java/HolaMundo/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Impresión con print:");
+        System.out.print("Hola");
+        System.out.print("Mucho gusto");
+        System.out.print("Mi nombre es Omar");
+
+        //Salto de línea
+        System.out.print("\n\n");
+
+        System.out.println("Impresión con print y caracter de escape:");
+        System.out.print("Hola\n");
+        System.out.print("Mucho gusto\n");
+        System.out.print("Mi nombre es Omar\n");
+
+        //Salto de línea
+        System.out.print("\n");
+
+        System.out.println("Impresión con println:");
+        System.out.println("Hola");
+        System.out.println("Mucho gusto");
+        System.out.println("Mi nombre es Omar");
+    }
+}
+
